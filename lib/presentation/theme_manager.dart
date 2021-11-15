17c1a3283cc0ec938347b86bb2060c7e51a239de
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2021-11-15 16:39:51 -0300

    [Lecture 10] Add card manager

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/presentation/color_manager.dart';
+import 'package:flutter_advanced_course/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
-      accentColor: ColorManager.grey,
-      
-      // card view theme
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    accentColor: ColorManager.grey,
 
-      // app bar theme
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
 
-      // button theme
+    // app bar theme
 
-      // text theme
+    // button theme
 
-      //input decoration theme (text form field)
+    // text theme
 
-      );
+    //input decoration theme (text form field)
+  );
 }
