6e95cbf80d8ee2fa8ffb7fb19b5105b7c7032f47
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2021-11-15 16:44:46 -0300

    [Lecture 11] Add App Bar theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_course/presentation/color_manager.dart';
+import 'package:flutter_advanced_course/presentation/font_manager.dart';
+import 'package:flutter_advanced_course/presentation/styles_manager.dart';
 import 'package:flutter_advanced_course/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primaryOpacity70,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
-    accentColor: ColorManager.grey,
+      // main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primaryOpacity70,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
+      accentColor: ColorManager.grey,
+      splashColor: ColorManager.primaryOpacity70,
+      // card view theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ),
 
-    // card view theme
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      shadowColor: ColorManager.grey,
-      elevation: AppSize.s4,
-    ),
+      // app bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primaryOpacity70,
+          titleTextStyle: getRegularStyle(
+            color: ColorManager.white,
+            fontSize: FontSize.s16
+          ))
+      // button theme
 
-    // app bar theme
+      // text theme
 
-    // button theme
-
-    // text theme
-
-    //input decoration theme (text form field)
-  );
+      //input decoration theme (text form field)
+      );
 }
