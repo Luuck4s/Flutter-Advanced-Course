7c791d0416628161bf264651659f4f27675102d6
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2021-11-15 21:44:18 -0300

    [Lecture 14] Add Text Form Field theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -6,67 +6,125 @@ import 'package:flutter_advanced_course/presentation/values_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.primaryOpacity70,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
-      accentColor: ColorManager.grey,
-      splashColor: ColorManager.primaryOpacity70,
-      // card view theme
-      cardTheme: CardTheme(
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    accentColor: ColorManager.grey,
+    splashColor: ColorManager.primaryOpacity70,
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.s4,
+    ),
+
+    // app bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s4,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(
         color: ColorManager.white,
-        shadowColor: ColorManager.grey,
-        elevation: AppSize.s4,
+        fontSize: FontSize.s16,
       ),
+    ),
 
-      // app bar theme
-      appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManager.primary,
-        elevation: AppSize.s4,
-        shadowColor: ColorManager.primaryOpacity70,
-        titleTextStyle:
-            getRegularStyle(color: ColorManager.white, fontSize: FontSize.s16),
+    // button theme
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
+      disabledColor: ColorManager.grey1,
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.primaryOpacity70,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(
+          color: ColorManager.white,
+        ),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(AppSize.s12),
+        ),
       ),
+    ),
 
-      // button theme
-      buttonTheme: ButtonThemeData(
-        shape: const StadiumBorder(),
-        disabledColor: ColorManager.grey1,
-        buttonColor: ColorManager.primary,
-        splashColor: ColorManager.primaryOpacity70,
+    // text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(
+        color: ColorManager.darkGrey,
+        fontSize: FontSize.s16,
       ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          textStyle: getRegularStyle(
-            color: ColorManager.white,
-          ),
-          primary: ColorManager.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(AppSize.s12),
-          ),
-        ),
+      subtitle1: getMediumStyle(
+        color: ColorManager.lightGrey,
+        fontSize: FontSize.s14,
       ),
+      caption: getRegularStyle(
+        color: ColorManager.grey1,
+      ),
+      bodyText1: getRegularStyle(
+        color: ColorManager.grey,
+      ),
+    ),
 
-      // text theme
-      textTheme: TextTheme(
-        headline1: getSemiBoldStyle(
-          color: ColorManager.darkGrey,
-          fontSize: FontSize.s16,
-        ),
-        subtitle1: getMediumStyle(
-          color: ColorManager.lightGrey,
-          fontSize: FontSize.s14,
-        ),
-        caption: getRegularStyle(
-          color: ColorManager.grey1,
-        ),
-        bodyText1: getRegularStyle(
+    //input decoration theme (text form field)
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+      hintStyle: getRegularStyle(
+        color: ColorManager.grey1,
+      ),
+      labelStyle: getMediumStyle(
+        color: ColorManager.darkGrey,
+      ),
+      errorStyle: getRegularStyle(
+        color: ColorManager.error,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
           color: ColorManager.grey,
+          width: AppSize.s1_5,
         ),
-      )
-
-      //input decoration theme (text form field)
-      );
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.error,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: ColorManager.primary,
+          width: AppSize.s1_5,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(
+            AppSize.s8,
+          ),
+        ),
+      ),
+    ),
+  );
 }
